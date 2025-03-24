fad24ad733ac2b9912bfa20a435f38c50d9943a9
JagratiDixit <jagratidixit2010@gmail.com>
2025-03-24 15:41:53 +0530

    UC2:Working edit,delete and add functionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms'; // ✅ Import FormsModule
+
 import { AddressBookComponent } from './components/address-book/address-book.component';
 
 @NgModule({
@@ -12,6 +14,7 @@ import { AddressBookComponent } from './components/address-book/address-book.com
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatTableModule,
     MatButtonModule,
     MatIconModule
